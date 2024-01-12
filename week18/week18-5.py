5b3c371ee9dc575e4e7ff08320e9d9f67ebe39ef
mumuneko <ingtrombone@gmail.com>
2024-01-12 11:32:25 +0800

    整理

---
+++ week18/week18-5.py
@@ -0,0 +1,11 @@
+#vhapter 3 資料儲存容器
+a=[100,200]#方括號list陣列、串列、列表
+c,d=a#逗號，左邊a,b叫tuple
+print(c)
+print(d)
+s='Hello'#字串
+t={100:200,300:400}#大括號 字典 裡面會有 左邊:右邊
+t={300}#大括號也可以是集合 裡面沒有冒號
+#chapter 4 if判斷
+#chapter 5 for迴圈 for i in range(N): or for c in s:
+#chapter 6 函式def gcd(a,b):這樣寫
